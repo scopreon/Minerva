5fb2b08309c7a645cd3337a9ea9a129f0d57a98d
scopreon (SC) <58375603+scopreon@users.noreply.github.com>
2022-10-17 12:49:33 +0100

    Update timetable.py
    Added checking for incorrect number of days

--- timetable.py
+++ timetable.py
@@ -2,6 +2,7 @@ from termcolor import colored
 import requests
 import json
 import datetime
+import sys
 
 JSESSIONID = ''
 cookies = {
@@ -21,21 +22,41 @@ headers = {
     'Sec-Fetch-Site': 'same-origin',
 }
 
+time = (datetime.datetime.today()).strftime('%Y-%m-%dT00:00:00.000+01:00')
 params = {
-    'start': '',
-    'limit': '',
+    'start': '{}'.format(time),
+    'limit': '50',
 }
 
-numberOfDays = int(input('Days: '))
+numberOfDays = 0
 
-response = requests.get('https://mytimetable.leeds.ac.uk/m/api/timetable', params=params, cookies=cookies, headers=headers)
+if len(sys.argv) > 1:
+    numberOfDays = sys.argv[1]
+else:
+    numberOfDays = input('Days: ')
+
+try:
+    numberOfDays = int(numberOfDays)
+    assert numberOfDays > 1
+except AssertionError:
+    print("Error {}: number must be greater than 0".format(numberOfDays))
+    exit(0)
+except ValueError:
+    print("Error {}: must be a number".format(numberOfDays))
+    exit(0)
+
+response = requests.get('https://mytimetable.leeds.ac.uk/m/api/timetable', params=params, cookies=cookies,
+                        headers=headers)
 with open("studentWithPrettyPrint3.json", "w") as write_file:
     json.dump(response.json(), write_file, indent=4, sort_keys=True)
 
-epoch_time = response.json()['data'][0]['startDate']/1000
+epoch_time = response.json()['data'][0]['startDate'] / 1000
 date_time = datetime.datetime.fromtimestamp(epoch_time)
 
-dates=[]
+dates = []
+
+# implement so you can ask for lots and it will keep making requests until you are done
+
 
 for x in response.json()['data']:
     epoch_time = x['startDate'] / 1000
@@ -47,6 +68,6 @@ for x in response.json()['data']:
             break
         print(date_time.strftime("%a %d %b:"))
     if len(dates) <= numberOfDays:
-        print(colored(date_time.strftime("%H:%M:"),'red'),end=' ')
-        print(colored(x['name'],'blue'), end=' ')
-        print(colored('in ' + x['locations'][0]['name'],'green'))
+        print(colored(date_time.strftime("%H:%M:"), 'red'), end=' ')
+        print(colored(x['name'], 'blue'), end=' ')
+        print(colored('in ' + x['locations'][0]['name'], 'green'))
